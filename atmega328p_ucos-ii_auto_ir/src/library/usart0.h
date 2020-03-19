c3d1e4dc08a850ea70d184d5c1f54cecb431259b
sonodasong <sonodasong@gmail.com>
2020-03-19 15:56:08 +0800

    Add source code and project file

---
+++ atmega328p_ucos-ii_auto_ir/src/library/usart0.h
@@ -0,0 +1,26 @@
+#ifndef __USART0_H__
+#define __USART0_H__
+
+#include "define.h"
+
+/* queue size is 2 ^ USART0_RX_Q_SIZE */
+#define USART0_RX_Q_SIZE		2
+/* buffer size is 2 ^ USART0_RX_SIZE */
+#define USART0_RX_SIZE			4
+#define USART0_RX_TIMEOUT		0xFFFFFFFF
+#define USART0_RX_EOF			'\r'
+
+#define USART0_TX_TIMEOUT		0xFFFFFFFF
+
+#define USART0_ENABLE_PRINTF		TRUE
+
+void usart0Init(void);
+INT8U usart0Read(char **str);
+INT8U usart0Print(char *str);
+#if USART0_ENABLE_PRINTF
+/* buffer size is 2 ^ USART0_TX_SIZE */
+#define USART0_TX_SIZE			4
+void usart0Printf(char *fmt, ...);
+#endif
+
+#endif
